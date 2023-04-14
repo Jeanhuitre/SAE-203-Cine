04ef521124a091cc6f839ac6f2cf39e7fcd775da
Jeanhuitre <91871206+Jeanhuitre@users.noreply.github.com>
2023-04-14 09:45:57 +0200

    connexion

---
+++ TD1__TP1_-_SAE_203/php/connexion.php
@@ -0,0 +1,13 @@
+<?php
+        $dsn="mysql:host=mysql.info.unicaen.fr;dbname=merino221_3";
+        $user="merino221";
+        $mdp="eequa4yaHei1ooph";
+
+        try {
+            $dtb=new PDO($dsn,$user,$mdp);
+            $dtb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "ERREUR : ".$e->getMessage()."<br>";
+            die();
+        }
+    ?>
